1ef19bad55f9a9d40a7c6f2e825eb12aa8d9fbf7
Comp1 <esergey05@gmail.com>
2023-09-17 15:21:51 +0300

    made port 4000

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ const { requestLogger, errorLogger } = require('./middlewares/Logger');
 const mainRouter = require('./routes/index');
 const NotFoundError = require('./errors/NotFoundError');
 
-// Слушаем 3000 порт
-const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
+// Слушаем 4000 порт
+const { PORT = 4000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 const app = express();
 app.use(cors());
 
